6a1de096bab5065f6ad10240acc5b20d002b7c8c
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-21 00:14:24 -0800

    add links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import harvardArt from './data/harvardArt';
+import GalleryNavigation from './components/GalleryNavigation/GalleryNavigation';
 
 function App() {
-  return <h1>Hello from App</h1>;
+  return (
+    <div>
+      <h1>Hello from App</h1>
+      <GalleryNavigation galleries={harvardArt.records} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/GalleryNavigation/GalleryNavigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function GalleryNavigation({ galleries }) {
+  console.log(
+    '🚀 ~ file: GalleryNavigation.js ~ line 4 ~ GalleryNavigation ~ galleries',
+    galleries
+  );
+  return (
+    <nav>
+      <NavLink to='/'>Home</NavLink>
+      <h1>GalleryNavigation</h1>
+      {galleries.map(gallery => (
+        <NavLink key={gallery.id} to={`/galleries/${gallery.galleryid}`}>
+          {gallery.name}
+        </NavLink>
+      ))}
+    </nav>
+  );
+}
+
+export default GalleryNavigation;
