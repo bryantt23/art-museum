52a39e3bae24842cf1c14d33450f0c61d6ba7049
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-21 01:56:18 -0800

    images with links

---
+++ src/components/ArtImageTile/ArtImageTile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+function ArtImageTile({ object }) {
+  const { galleryId } = useParams();
+  console.log(
+    '🚀 ~ file: ArtImageTile.js ~ line 6 ~ ArtImageTile ~ galleryId',
+    galleryId
+  );
+  console.log(
+    '🚀 ~ file: ArtImageTile.js ~ line 4 ~ ArtImageTile ~ object',
+    object
+  );
+  const { images, id } = object;
+  return (
+    <div>
+      <Link to={`/galleries/${galleryId}/art/${id}`}>
+        <img style={{ height: 100 }} src={images[0]['baseimageurl']} />
+      </Link>
+      ArtImageTile
+      {JSON.stringify(object.images)}
+      <p></p>
+    </div>
+  );
+}
+
+export default ArtImageTile;
--- src/components/GalleryView/GalleryView.js
+++ src/components/GalleryView/GalleryView.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useParams, Switch, Route } from 'react-router-dom';
+import ArtImageTile from '../ArtImageTile/ArtImageTile';
 
 function GalleryView({ galleries }) {
   const { galleryId } = useParams();
   const targetGallery = galleries.find(
     gallery => gallery.gallerynumber === galleryId
   );
+  console.log(
+    '🚀 ~ file: GalleryView.js ~ line 10 ~ GalleryView ~ targetGallery',
+    targetGallery
+  );
   return (
     <div>
       <h1>Hello from GalleryView</h1>
       <h2>{targetGallery.name}</h2>
+      {targetGallery.objects.map(object => (
+        <ArtImageTile object={object} />
+      ))}
     </div>
   );
 }
