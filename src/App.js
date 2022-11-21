7f0323549b50764282251b34d14bf7c71dde4186
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-21 01:26:24 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -9,8 +9,21 @@ function App() {
       <h1>Hello from App</h1>{' '}
       <GalleryNavigation galleries={harvardArt.records} />
       <Switch>
+        <Route exact path='/'>
+          <div>
+            <h2>Harvard Art Museum</h2>
+            <p>
+              Look, but Don't Touch. Please select a Gallery in the navigation
+              bar. Please select a Gallery in the navigation bar.
+            </p>
+          </div>
+        </Route>
+
         <Route path={'/galleries/:galleryId'}>
           <GalleryView galleries={harvardArt.records} />
+        </Route>
+        <Route>
+          <div>Page Not Found</div>
         </Route>
       </Switch>
     </div>
